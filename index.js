4f4695ca67e681cd36e3085557f90bba95a5473f
Peter Bacenet <peterbacenet@icloud.com>
2021-08-17 20:24:58 -0400

    commit tree fe0bba6be20a2cedfbf27e6bb1e35ab38b0a1148

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code in this file!
+const currentUser = 'Peter Bacenet' ;
+const welcomeMessage = `Welcome to Flatbook, ${currentUser} !`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase(); //() invoked it to make it work immediately
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!` //0->1 to take the first intitial
+console.log(shortGreeting)
